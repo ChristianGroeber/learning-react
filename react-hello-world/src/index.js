b6963e9a77ca31e0f13add2a59a164fce95c7d41
christiangroeber <christian.groeber@bluewin.ch>
2019-05-14 19:40:36 +0200

    my first steps using components and props

--- react-hello-world/src/index.js
+++ react-hello-world/src/index.js
@@ -1,43 +1,12 @@
 import React from 'react';
 import * as ReactDom from "react-dom";
 
-let person = {
-    title: 'General',
-    name: 'Kenobi',
-    age: 5000
-};
-
-let villain = {
-    title: 'General',
-    name: 'Grievous'
-};
-
-let niceMan = {
-    title: "Jedi Master",
-    name: "Yoda"
-};
-
-const formatUser = (person) => {
-    return person.title + " " + person.name;
-};
-
-const greetTheNiceMan = (villain) => {
-    if (villain.name === 'Grievous'){
-        return "Hello there";
-    }else{
-        return "Wut?";
+class Welcome extends React.Component{
+    render(){
+        return <h1>Hello there - {this.props.title} {this.props.name}</h1>;
     }
-};
+}
 
-let count = 1;
-const tick = () => {
-    const element = (
-        <div className={person.name}>
-            <h2>{count}</h2>
-        </div>
-    );
-    count++;
-    ReactDom.render(element, document.getElementById('root'));
-};
+const element = <Welcome title="General" name="Kenobi"/>;
 
-setInterval(tick, 1000);
+ReactDom.render(element, document.getElementById('root'));
